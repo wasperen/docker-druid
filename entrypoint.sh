bed92a03bfb956c5534d8fd0a02df4e04219585e
Willem van Asperen <willem@van.asperen.org>
2017-04-10 07:42:14 +0200

    simplification of entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 set -e
 
-# java options
-JO_HISTORICAL = "conf/druid/historical/jvm.conf"
-JO_BROKER     = "conf/druid/broker/jvm.conf"
-JO_COORDINATOR= "conf/druid/coordinator/jvm.conf"
-JO_OVERLORD   = "conf/druid/overlord/jvm.conf"
-JO_MIDDLEMGR  = "conf/druid/middleManager/jvm.conf"
-JO_EXTRA      = ""
+# general options
+CONF_DIR      = "conf"
+ENVIRONMENT   = "druid"
 
 # class paths
 CP_COMMON     = "conf/druid/_common"
-CP_HISTORICAL = "conf/druid/historical"
-CP_BROKER     = "conf/druid/broker"
-CP_COORDINATOR= "conf/druid/coordinator"
-CP_OVERLORD   = "conf/druid/overlord"
-CP_MIDDLEMGR  = "conf/druid/moddleManager"
 CP_LIB        = "lib/*"
 CP_EXTRA      = ""
 
@@ -35,30 +26,7 @@ if [ "$1" = 'init'] || [! -d "var" ]; then
 	fi
 fi
 
-# services to start
-if [ "$1" = 'historical']; then
-	JAVA_OPTIONS = $JO_HISTORICAL
-	CLASS_PATH = "$CP_COMMON:$CP_HISTORICAL:$CP_LIB:$CP_EXTRA"
-fi
-
-if [ "$1" = 'broker']; then
-	JAVA_OPTIONS = $JO_BROKER
-	CLASS_PATH = "$CP_COMMON:$CP_BROKER:$CP_LIB:$CP_EXTRA"
-fi
-
-if [ "$1" = 'coordinator']; then
-	JAVA_OPTIONS = $JO_COORDINATOR
-	CLASS_PATH = "$CP_COMMON:$CP_COORDINATOR:$CP_LIB:$CP_EXTRA"
-fi
-
-if [ "$1" = 'overlord']; then
-	JAVA_OPTIONS = $JO_OVERLORD
-	CLASS_PATH = "$CP_COMMON:$CP_OVERLORD:$CP_LIB:$CP_EXTRA"
-fi
-
-if [ "$1" = 'middleManager']; then
-	JAVA_OPTIONS = $JO_MIDDLEMGR
-	CLASS_PATH = "$CP_COMMON:$CP_MIDDLEMGR:$CP_LIB:$CP_EXTRA"
-fi
+JAVA_OPTIONS = "$CONF_DIR/$ENVIRONMENT/$1/jvm.conf"
+CLASS_PATH   = "$CP_COMMON:$CONF_DIR/$ENVIRONMENT/$1:$CP_LIB:$CP_EXTRA"
 
 exec java $(cat $JAVA_OPTIONS) | xargs -cp $CLASS_PATH $CLASS_DRUID server $1
